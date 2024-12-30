badeedbf8e577c95558734065e4d3f884f8f87f2
reishash <athaya27ramdhany@gmail.com>
2024-12-30 18:52:52 +0700

    Add background images for additional scene IDs to enhance visual experience

--- src/Source/Scenes/Scene.java
+++ src/Source/Scenes/Scene.java
@@ -556,6 +556,17 @@ public class Scene extends JFrame {
         // Set background image
         String bgPath;
         switch (sceneID) {
+            case 3, 4, 5, 6, 7, 8, 9:
+                bgPath = "src/Assets/Images/bg_roomelara.jpg";
+                break;
+            case 10, 11, 12, 13, 14, 15:
+                bgPath = "src/Assets/Images/bg_code.jpg";
+                break;
+            case 16, 17:
+                bgPath = "src/Assets/Images/bg_glitch.jpg";
+            case 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37:
+                bgPath = "src/Assets/Images/bg_nullspace.jpg";
+                break;
             default:
                 bgPath = "";
                 break;
