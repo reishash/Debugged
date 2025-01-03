894321ed6ed67770761e124a200cb3383f98f645
reishash <athaya27ramdhany@gmail.com>
2025-01-03 15:59:24 +0700

    Remove unused imports in SelectChapter class to clean up code

--- src/Source/Scenes/SelectChapter.java
+++ src/Source/Scenes/SelectChapter.java
@@ -6,8 +6,6 @@ import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Font;
@@ -23,7 +21,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.AbstractAction;
 import javax.swing.ActionMap;
 import javax.swing.ImageIcon;
